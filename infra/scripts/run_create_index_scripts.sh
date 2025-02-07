a4f594777993c8f27c45b0132ef557f5eca672b9
Pavan-Microsoft <v-kupavan@microsoft.com>
2025-02-07 18:49:36 +0530

    add ckm-analyzer file path

--- infra/scripts/run_create_index_scripts.sh
+++ infra/scripts/run_create_index_scripts.sh
@@ -15,6 +15,9 @@ curl --output "02_create_cu_template_text.py" ${baseUrl}"infra/scripts/index_scr
 curl --output "02_create_cu_template_audio.py" ${baseUrl}"infra/scripts/index_scripts/02_create_cu_template_audio.py"
 curl --output "03_cu_process_data_text.py" ${baseUrl}"infra/scripts/index_scripts/03_cu_process_data_text.py"
 curl --output "content_understanding_client.py" ${baseUrl}"infra/scripts/index_scripts/content_understanding_client.py"
+curl --output "ckm-analyzer_config_audio.json" ${baseUrl}"infra/data/ckm-analyzer_config_audio.json"
+curl --output "ckm-analyzer_config_text.json" ${baseUrl}"infra/data/ckm-analyzer_config_text.json"
+curl --output "audio_data" ${baseUrl}"infra/data/audio_data"
 
 # RUN apt-get update
 # RUN apt-get install python3 python3-dev g++ unixodbc-dev unixodbc libpq-dev
