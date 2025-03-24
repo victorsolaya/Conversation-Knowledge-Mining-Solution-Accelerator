f159e42737da5dc0a844330d383b58dcde9f0919
Pavan-Microsoft <v-kupavan@microsoft.com>
2025-03-24 20:59:53 +0530

    logger

--- src/api/services/chart_service.py
+++ src/api/services/chart_service.py
@@ -4,6 +4,9 @@ from fastapi import HTTPException, status
 from api.models.input_models import ChartFilters
 from common.database.sqldb_service import adjust_processed_data_dates, fetch_chart_data, fetch_filters_data
 
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 class ChartService:
     """
@@ -18,7 +21,7 @@ class ChartService:
             adjust_processed_data_dates()
             return fetch_filters_data()
         except Exception as e:
-            logging.error("Error in fetch_filter_data: %s", e, exc_info=True)
+            logger.error("Error in fetch_filter_data: %s", e, exc_info=True)
             raise HTTPException(
                 status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
                 detail="An error occurred while fetching filter data."
@@ -31,7 +34,7 @@ class ChartService:
         try:
             return await fetch_chart_data()
         except Exception as e:
-            logging.error("Error in fetch_chart_data: %s", e, exc_info=True)
+            logger.error("Error in fetch_chart_data: %s", e, exc_info=True)
             raise HTTPException(
                 status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
                 detail="An error occurred while fetching chart data."
@@ -44,7 +47,7 @@ class ChartService:
         try:
             return await fetch_chart_data(chart_filters)
         except Exception as e:
-            logging.error("Error in fetch_chart_data_with_filters: %s", e, exc_info=True)
+            logger.error("Error in fetch_chart_data_with_filters: %s", e, exc_info=True)
             raise HTTPException(
                 status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
                 detail="An error occurred while fetching filtered chart data."
