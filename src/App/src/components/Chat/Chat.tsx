294159c95d2e30c22f032341ba37d0e693e27cde
Rohini-Microsoft <v-rwalunj@microsoft.com>
2025-04-04 17:33:38 +0530

    handled rate limit error frontend

--- src/App/src/components/Chat/Chat.tsx
+++ src/App/src/components/Chat/Chat.tsx
@@ -73,7 +73,8 @@ const Chat: React.FC<ChatProps> = ({
       payload: true,
     });
 
-    if ((reqType !== 'graph' && reqType !== 'error') && isCharthDisplayDefault){
+    if (((reqType !== 'graph' && reqType !== 'error')  ||  messages[messages.length - 1].role !== ERROR) && isCharthDisplayDefault ){
+
       setIsChartLoading(true);
       setTimeout(()=>{
         makeApiRequestForChart('show in a graph by default', convId, messages[messages.length - 1].content as string)
@@ -267,7 +268,7 @@ const Chat: React.FC<ChatProps> = ({
           const errorMsg = JSON.parse(runningText).error;
           const errorMessage: ChatMessage = {
             id: generateUUIDv4(),
-            role: ASSISTANT,
+            role: ERROR,
             content: errorMsg,
             date: new Date().toISOString(),
           };
@@ -332,7 +333,7 @@ const Chat: React.FC<ChatProps> = ({
                 parsedChartResponse?.object?.message;
               const errorMessage: ChatMessage = {
                 id: generateUUIDv4(),
-                role: ASSISTANT,
+                role: ERROR,
                 content: errorMsg,
                 date: new Date().toISOString(),
               };
@@ -546,7 +547,7 @@ const Chat: React.FC<ChatProps> = ({
           
           const errorMessage: ChatMessage = {
             id: generateUUIDv4(),
-            role: ASSISTANT,
+            role: ERROR,
             content: errorMsg,
             date: new Date().toISOString(),
           };
@@ -612,7 +613,7 @@ const Chat: React.FC<ChatProps> = ({
                 parsedChartResponse?.object?.message;
               const errorMessage: ChatMessage = {
                 id: generateUUIDv4(),
-                role: ASSISTANT,
+                role: ERROR,
                 content: errorMsg,
                 date: new Date().toISOString(),
               };
