8faade76f86ec9005db85d53da1a17d8ad619b4e
Pavan-Microsoft <v-kupavan@microsoft.com>
2025-04-23 16:00:45 +0530

    Deploy AI model manually

---
+++ infra/scripts/deploy_ai_model.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Variables
+DEPLOYMENT_NAME="ai-model-deployment"
+BICEP_FILE="../deploy_ai_model.bicep"
+
+# Parameters
+RESOURCE_GROUP_NAME="$1"
+SOLUTION_NAME="$2"
+DEPLOYMENT_TYPE="$3"
+GPT_MODEL_NAME="$4"
+GPT_DEPLOYMENT_CAPACITY="$5"
+EMBEDDING_MODEL="$6"
+EMBEDDING_DEPLOYMENT_CAPACITY="$7"
+
+# Execute the Bicep deployment
+az deployment group create \
+  --resource-group $RESOURCE_GROUP_NAME \
+  --name $DEPLOYMENT_NAME \
+  --template-file $BICEP_FILE \
+  --parameters solutionName=$SOLUTION_NAME \
+               deploymentType=$DEPLOYMENT_TYPE \
+               gptModelName=$GPT_MODEL_NAME \
+               gptDeploymentCapacity=$GPT_DEPLOYMENT_CAPACITY \
+               embeddingModel=$EMBEDDING_MODEL \
+               embeddingDeploymentCapacity=$EMBEDDING_DEPLOYMENT_CAPACITY
