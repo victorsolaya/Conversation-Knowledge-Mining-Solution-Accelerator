44538746eca17bd27c237c4226fd583ff87a193b
Kanchan Nagshetti (Persistent Systems Inc) <v-knagshetti@microsoft.com>
2025-05-16 12:17:14 +0530

    test case

--- src/tests/api/helpers/test_chat_helper.py
+++ src/tests/api/helpers/test_chat_helper.py
@@ -7,7 +7,7 @@ import sys
 import os
 
 # Add the project root to the path so we can import the module under test
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../../..")))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../../../api")))
 
 from src.api.helpers.chat_helper import process_rag_response, complete_chat_request
 
